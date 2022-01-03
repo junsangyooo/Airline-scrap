73c61ce2a2cb52ef430f98851c88ea395ddedfa0
Jun Yoo <dbwnstkddb@gmail.com>
2022-01-03 14:09:33 -0500

    Koreanair Selenium

--- Koreanair.py
+++ Koreanair.py
@@ -4,41 +4,51 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
-## 여행정보 input 받기
-round_trip = bool(input("Round trip? True/False "))
-leaving = str(input("From where? ICN/YYZ "))
-arriving = str(input("To where? ICN/YYZ "))
-departure = input("When is your departure date? YYYYMMDD ")
-come_back = input("When is your return date? If not round trip, just leave 'a'. YYYYMMDD ")
+## Getting Input Data for Flight
+# round_trip = bool(input("Round trip? True/False "))
+# leaving = input("From where? ICN/YYZ ")
+# arriving = input("To where? ICN/YYZ ")
+# departure = input("When is your departure date? YYYYMMDD ")
+# come_back = input("When is your return date? If not round trip, just leave 'a'. YYYYMMDD ")
+round_trip = True
+leaving = "YYZ"
+arriving = "ICN"
+departure = 20220108
+come_back = 20220824
 
-## 웹을오픈하지않고 안에서 돌리는거로 설정
+## Set Webdriver Options
 options = webdriver.ChromeOptions()
 ## options.headless = True
+options.add_argument("start-maximized")
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")
-
-kurl = "https://www.koreanair.com/kr/ko"
-# curl = "https://www.aircanada.com/kr/en/aco/home.html"
-
 kdriver = webdriver.Chrome(options=options)
-# cdriver = webdriver.Chrome(options=options)
-# cdriver.maximize_window
+kdriver.maximize_window
 
-## Selenium 사용해서 웹 실행
+## Run Web Using Selenium
+kurl = "https://www.koreanair.com/kr/ko"
 kdriver.get(kurl)
-# cdriver.get(curl)
 
 
-## 출발지 도착지 설정
+## Set From:
 kdriver.find_element(By.CLASS_NAME, "quickbooking__location.-from").click()
-kdriver.find_element(By.XPATH, "/html/body/ke-dynamic-modal/div/ke-airport-layer/div/div/div/div/ke-airport-chooser/div/div[2]/ul").send_keys(leaving)
-kdriver.find_element(By.CLASS_NAME, "combobox__item.auto-complete-item").click()
-kdriver.find_element(By.CLASS_NAME, "quickbooking__location.-to").click()
+kdriver.find_element(By.XPATH, "/html/body/ke-dynamic-modal/div/ke-airport-layer/div/div/div/div/ke-airport-chooser/div/div[1]/input").send_keys(leaving)
+leave = WebDriverWait(kdriver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#item0")))
+leave.click()
+
+
+## Set To:
+kdriver.find_element(By.XPATH, "/html/body/div[7]/div[2]/div/div/div[2]/ke-main/ke-quick-booking/div/div/div/div/div[1]/ke-quick-booking-ow-rt/div[1]/div/ke-airport-selector/button[2]").click()
 kdriver.find_element(By.XPATH, "/html/body/ke-dynamic-modal/div/ke-airport-layer/div/div/div/div/ke-airport-chooser/div/div[1]/input").send_keys(arriving)
-kdriver.find_element(By.CLASS_NAME, "combobox__item.auto-complete-item").click()
+arrive = WebDriverWait(kdriver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#item0")))
+arrive.click()
 
 
-## 출국일 입국일 설정
+## Set Departure and Return
 kdriver.find_element(By.CLASS_NAME, "quickbooking__datepicker").click()
 if round_trip:
     kdriver.find_element(By.ID, "ipt-depature").send_keys(departure)
